aa9d04f7d490e55a399d23bcce36015b02e4324e
Thananan <thananan.pl@gmail.com>
2025-10-27 23:56:04 +0700

    fix: route

--- services/user-management-service/internal/adapters/db/models.go
+++ services/user-management-service/internal/adapters/db/models.go
@@ -1,7 +1,9 @@
 package db
 
+// Model for saving into gorm
 type UserModel struct {
-	ID       string `gorm:"primaryKey"`
-	Email    string `gorm:"uniqueIndex"`
-	FullName string
+	UserID   string `bson:"user_id,omitempty" gorm:"primaryKey"`
+	FullName string `bson:"fullname"`
+	Email    string `bson:"email"`
+	Role     string `bson:"role"` // "prophet" or "customer"
 }
