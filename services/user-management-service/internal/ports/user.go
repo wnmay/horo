b047af1f30f641577246917ad257a20a8fa572d5
Flook0147 <flook.akkharaphon@gmail.com>
2025-11-05 17:16:34 +0700

    getMe

--- services/user-management-service/internal/ports/user.go
+++ services/user-management-service/internal/ports/user.go
@@ -2,8 +2,11 @@ package ports
 
 import (
 	"context"
+
+	"github.com/wnmay/horo/services/user-management-service/internal/domain"
 )
 
 type UserManagementService interface {
 	Register(ctx context.Context, idToken, fullName, role string) error
+	GetMe(ctx context.Context, userId string) (*domain.User, error)
 }
