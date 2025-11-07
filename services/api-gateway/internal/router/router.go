6329618413eedd8c9a7c4f3edd1c75790236d626
chera02 <thananan.it@topgunthailand.com>
2025-11-08 01:00:13 +0700

    fix: ws auth middleware to extract from token

--- services/api-gateway/internal/router/router.go
+++ services/api-gateway/internal/router/router.go
@@ -126,7 +126,7 @@ func (r *Router) setupWebsocketRoutes() {
 	chatPublisher := publishers.NewChatMessagePublisher(r.rmq)
 	chatWsHandler := ws_handler.NewChatWSHandler(r.hub, chatPublisher)
 
-	r.app.Use("/ws/chat", r.authMiddleware.AddClaims, func(c *fiber.Ctx) error {
+	r.app.Use("/ws/chat", r.authMiddleware.AddClaimsWS, func(c *fiber.Ctx) error {
 		if websocket.IsWebSocketUpgrade(c) {
 			return c.Next()
 		}
