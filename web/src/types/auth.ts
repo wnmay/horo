84d0fac1cc38a99222ce66bd56ff2b0c12862ed6
wnmay <wanathachat@gmail.com>
2025-11-10 10:26:18 +0700

    fix: edit firebase claims

--- web/src/types/auth.ts
+++ web/src/types/auth.ts
@@ -3,7 +3,5 @@ import { JwtPayload } from "jwt-decode";
 export interface FirebaseClaims extends JwtPayload {
   user_id: string;
   email: string;
-  name?: string;
   role?: string;
-  picture?: string;
 }
